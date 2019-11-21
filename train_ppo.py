389e8ae51b5cc37a9064bbb2798175ec57b04ded
minaek <mk898@cornell.edu>
2019-11-20 23:29:09 -0800

    set up wandb and data saving

--- train_ppo.py
+++ train_ppo.py
@@ -1,3 +1,8 @@
+import os
+import collections
+import pickle
+import shutil
+import csv
 import gym
 import numpy as np
 import time
@@ -11,38 +16,50 @@ import driving_envs
 
 FLAGS = flags.FLAGS
 # PPO parameters
-flags.DEFINE_integer("timesteps", 100000, "# timesteps to train")
+flags.DEFINE_integer("timesteps", 1000000, "# timesteps to train")
 # Experiment related parameters
-flags.DEFINE_string("name", "test", "Name of experiment")
-flags.DEFINE_string("logdir", "./test", "logdir")
-flags.DEFINE_boolean("is_save", False, "Saves and logs experiment data if True")
-flags.DEFINE_integer("eval_save_period", 10, "how often we save state for eval")
+flags.DEFINE_string("name", "safe0", "Name of experiment")
+flags.DEFINE_boolean("is_save", True, "Saves and logs experiment data if True")
+flags.DEFINE_integer("eval_save_period", 100, "how often we save state for eval")
 
 
-def train(timesteps, experiment_name, is_save, eval_save_period, log_dir, num_envs=1):
+def train(timesteps, experiment_name, is_save, eval_save_period, num_envs=1):
+    if is_save:
+        if os.path.exists(experiment_name):
+            shutil.rmtree(experiment_name)
+        os.makedirs(experiment_name)
+        rets_path = os.path.join(experiment_name, "eval.csv")
+        wandb.save(experiment_name)
+
     env_fns = num_envs * [lambda: gym.make("Merging-v0")]
     env = VecNormalize(SubprocVecEnv(env_fns))
     eval_env = VecNormalize(DummyVecEnv(env_fns), training=False, norm_reward=False)
     policy = MlpPolicy
     model = PPO2(policy, env, verbose=1)
 
-    def evaluate(model):
+    def evaluate(model, eval_dir=None):
         """
         Evaluates model on one episode of driving task. Returns mean episode reward.
         """
         rets = 0.0
         obs = eval_env.reset()
         state, ever_done = None, False
+        task_data = []
         while not ever_done:
             action, state = model.predict(obs, state=state, deterministic=True)
-            print("\n" + str(action))
+            #print("\naction: ", action)
             next_obs, rewards, done, _info = eval_env.step(action)
-            eval_env.render()
+            if not is_save: eval_env.render()
             if not ever_done:
+                task_data.append([eval_env.venv.envs[0].world.state, action, rewards, done])
                 rets += rewards
             ever_done = np.logical_or(ever_done, done)
             obs = next_obs
-            time.sleep(.1)
+            if not is_save: time.sleep(.1)
+        if is_save:
+            assert eval_dir
+            with open(os.path.join(eval_dir, "task_data.pkl"), "wb") as f:
+                pickle.dump(task_data, f)
         return np.mean(rets)
 
     n_steps = 0
@@ -52,16 +69,35 @@ def train(timesteps, experiment_name, is_save, eval_save_period, log_dir, num_en
         model = _locals['self']
         if (n_steps + 1) % eval_save_period == 0:
             start_eval_time = time.time()
-            mean_ret = evaluate(model)
-            print("mean eval ret: ", mean_ret)
+            if is_save:
+                eval_dir = os.path.join(experiment_name, "eval{}".format(n_steps))
+                os.makedirs(eval_dir)
+                ret = evaluate(model, eval_dir)
+            else: ret = evaluate(model)
+            print("eval ret: ", ret)
+            if is_save:
+                wandb.log({"eval_ret": ret}, step=_locals["self"].num_timesteps)
+                with open(rets_path, "a", newline="") as f:
+                    writer = csv.writer(f)
+                    writer.writerow([n_steps, ret])
             end_eval_time = time.time() - start_eval_time
             print("Finished evaluation in {:.2f} seconds".format(end_eval_time))
         n_steps += 1
         return True
 
     model.learn(total_timesteps=timesteps, callback=callback)
+    if is_save:
+        final_dir = os.path.join(experiment_name, "eval{}".format(n_steps))
+        os.makedirs(final_dir)
+        ret = evaluate(model, final_dir)
+        with open(rets_path, "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([n_steps, ret])
+        model.save(experiment_name)
+    else:
+        evaluate(model)
 
 
 if __name__ == '__main__':
     if FLAGS.is_save: wandb.init(project="reward_adaptation", sync_tensorboard=True)
-    train(FLAGS.timesteps, FLAGS.name, FLAGS.is_save, FLAGS.eval_save_period, FLAGS.logdir)
+    train(FLAGS.timesteps, FLAGS.name, FLAGS.is_save, FLAGS.eval_save_period)
