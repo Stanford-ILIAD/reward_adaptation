517a6dbd8f95718262a4613d5c106d4f1714761e
minaek <mk898@cornell.edu>
2019-12-03 15:43:29 -0800

    remove render and saving in evaluate_debug

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pickle
 
 
-def evaluate_debug(model, eval_env, is_save, eval_dir=None):
+def evaluate_debug(model, eval_env):
         """
         Evaluates model on one episode of driving task. Returns mean episode reward.
         """
@@ -15,22 +15,14 @@ def evaluate_debug(model, eval_env, is_save, eval_dir=None):
         task_data = []
         while not ever_done:
                 action, state = model.predict(obs, state=state, deterministic=True)
-                #print("\naction: ", action)
                 next_obs, rewards, done, _info = eval_env.step(action)
-                print("rewards: ", rewards)
-                #if not is_save: eval_env.render()
-                eval_env.render()
+                #eval_env.render()
                 if not ever_done:
                         task_data.append([eval_env.venv.envs[0].world.state, action, rewards, done])
                         rets += rewards
                 ever_done = np.logical_or(ever_done, done)
                 obs = next_obs
-                #if not is_save: time.sleep(.1)
-                time.sleep(.1)
-        if is_save:
-                assert eval_dir
-                with open(os.path.join(eval_dir, "task_data.pkl"), "wb") as f:
-                        pickle.dump(task_data, f)
+                #time.sleep(.1)
         return rets
 
 
