e722693682fbe0ee65416f321f23522dfa223d43
minaek <mk898@cornell.edu>
2020-01-27 19:22:42 -0800

    erased expt directory vars in eval_model

--- eval_model.py
+++ eval_model.py
@@ -152,46 +152,10 @@ def eval_weight_similarity(model1, model2):
 
 
 if __name__ == "__main__":
-
-    eff = ("eff", "best_model_16640_386.66973876953125.pkl")
-    safe = ("safe", "best_model_298240_540.3814697265625.pkl")
-    safe = ("safe", "best_model_247040_257.32415771484375.pkl")
-    eff_lite = ("eff_lite", "best_model_92160_-32.7154655456543.pkl")
-    safe_lite = ("safe_lite", "best_model_1280_-56.451454162597656.pkl")
-    safe2eff = ("0.2safe_eff", "final_model_9.pkl")
-    eff2safe = ("0.2eff_safe", "final_model_9.pkl")
-
-    weight_n1 = ("weight_-1", "best_model_151040_[710.741].pkl")
-    weight_n05 = ("weight_-0.5", "best_model_1428480_[309.0573].pkl")
-    weight_0 = ("weight_0", "best_model_87040_[-37.172234].pkl")
-    weight_p05 = ("weight_0.5", "best_model_16640_[-33.3783].pkl")
-    weight_p1 = ("weight_1", "best_model_8960_[-29.448769].pkl")
-    weight_p2 = ("weight_2", "best_model_1980160_[1087.1274].pkl")
-    weight_p4 = ("weight_4", "best_model_1827840_[2262.8826].pkl")
-    weight_p6 = ("weight_6", "best_model_2670080_[3432.1975].pkl")
-    weight_p8 = ("weight_8", "best_model_3175680_[4600.899].pkl")
-    weight_p10 = ("weight_10", "best_model_3527680_[5769.4253].pkl")
-    weight_p100 = ("weight_100", "best_model_14080_[58647.805].pkl")
-
-    weight_p1 = ("weight_1", "best_model_72960_15906.4873046875.pkl")
-    weight_p100 = ("weight_100", "best_model_30720_778549.9375.pkl")
-    weight_p100_free = ("weight_100_free", "best_model_1280_521154.40625.pkl")
-    weight_p10 = ("weight_10", "best_model_180480_935024.125.pkl")
-    weight_p05 = ("weight_0.5", "ckpt_model_160000_253363.203125.pkl")
-    weight_0 = ("weight_0", "best_model_33280_899905.375.pkl")
-    weight_n05 = ("weight_-0.5", "best_model_125440_906951.0.pkl")
-    weight_n1 = ("weight_-1", "best_model_67840_827945.125.pkl")
-    weight_n10 = ('weight_-10', 'best_model_74240_940575.6875.pkl')
-
-    w1 = ("1", "final_model_0.pkl")
-    w1000 = ("1000", "best_model_473600_6036.03759765625.pkl")
-    w10 = ("10", "best_model_844800_5182.9755859375.pkl")
-    w1000norm = ('1000_norm', 'best_model_38400_0.06706708669662476.pkl')
-    w100norm = ('100_norm', 'best_model_806400_546.5267944335938.pkl')
-    w10norm = ('10_norm', 'best_model_806400_474.72021484375.pkl')
-    model = w100norm
-    model_dir = os.path.join("monotonic", model[0], model[1])
-    eval_env = load_env("Merging-v8")
+    n1 = ("sample_complexity", "-1", "best_model_192000_-32.513710021972656.pkl")
+    model = n1
+    model_dir = os.path.join(model[0], model[1], model[2])
+    eval_env = load_env("Merging-v0")
 
     #model = n1_p10
     #model_dir = os.path.join("merging", model[0], model[1])
