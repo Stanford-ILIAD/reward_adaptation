c4ab7913c525bc076b60078dd9c9062e09764d1e
minaek <mk898@cornell.edu>
2020-02-05 09:27:17 -0800

    updating eval_model

--- eval_model.py
+++ eval_model.py
@@ -13,6 +13,8 @@ from stable_baselines.common.vec_env.vec_normalize import VecNormalize
 import wandb
 from tensorflow import flags
 import minigrid.gym_minigrid
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 def load_env(env, num_envs=1):
     #env_fns = num_envs * [lambda: gym.make(env)]
@@ -43,21 +45,43 @@ def evaluate(model, eval_env):
             action, state = model.predict(obs, state=state, deterministic=True)
             next_obs, ret, done, _info = eval_env.step(action, verbose=False)
             #print("ret: ", ret)
-            #eval_env.render()
+            eval_env.render()
             if not ever_done:
                 rets += ret
             #print("rets: ", rets)
             obs = next_obs
-            #time.sleep(.1)
+            time.sleep(.1)
             ever_done = done
         total_rets.append(rets)
-        #print("total mean ep return: ", np.mean(total_rets), total_rets)
-        #print("nsteps: ", nsteps)
+        print("total mean ep return: ", np.mean(total_rets), total_rets)
+        print("nsteps: ", nsteps)
     return np.mean(total_rets), np.std(total_rets), total_rets
 
 if __name__ == "__main__":
-    gw1 = ("gridworld", "v0", "best_model_5000_101.0.pkl")
-    model = gw1
+    h2v0 = ("gridworld", "h2_v0", "best_model_5000_11.62.pkl")
+    h2v1 = ("gridworld", "h2_v1", "best_model_88000_11.559999999999999.pkl")
+    h1v0 = ("gridworld", "h1_v0", "best_model_6000_11.62.pkl")
+    h1v1 = ("gridworld", "h1_v1", "best_model_17000_11.52.pkl")
+
+    h2v1_h2v0 = ("gridworld", "h2v1_h2v0", "best_model_22000_11.62.pkl")
+    h1v0_h2v0 = ("gridworld", "h1v0_h2v0", "best_model_27000_11.62.pkl")
+    h1v1_h2v0 = ("gridworld", "h1v1_h2v0", "best_model_108000_11.62.pkl")
+
+    h2v0_h2v1 = ("gridworld", "h2v0_h2v1", "best_model_2000_11.58.pkl")
+    h1v0_h2v1 = ("gridworld", "h1v0_h2v1", "best_model_46000_10.4.pkl")
+    h1v1_h2v1 = ("gridworld", "h1v1_h2v1", "best_model_46000_10.4.pkl")
+
+    h2v0_h1v0 = ('gridworld', 'h2v0_h1v0', 'best_model_2000_10.34.pkl')
+    h2v1_h1v0 = ('gridworld', 'h2v1_h1v0', 'best_model_25000_11.62.pkl')
+    h1v1_h1v0 = ('gridworld', 'h1v1_h1v0', 'best_model_16000_11.62.pkl')
+
+    h2v0_h1v1 = ('gridworld', 'h2v0_h1v1', 'best_model_5000_11.54.pkl')
+    h2v0_h1v1 = ('gridworld', 'h2v0_h1v1', 'best_model_8000_11.559999999999999.pkl')
+    h2v1_h1v1 = ('gridworld', 'h2v1_h1v1', 'best_model_27000_11.52.pkl')
+    h2v1_h1v1 = ('gridworld', 'h2v1_h1v1', 'best_model_29000_11.58.pkl')
+    h1v0_h1v1 = ('gridworld', 'h1v0_h1v1', 'best_model_7000_11.52.pkl')
+
+    model = h2v0
     model_dir = os.path.join(model[0], model[1], model[2])
     eval_env = load_env("Gridworld-v0")
 
@@ -67,6 +91,6 @@ if __name__ == "__main__":
     for ne in range(num_episode):
         mean_ret, std_ret, total_ret = evaluate(model, eval_env)
         sum_reward += mean_ret
-        print("running mean: ", sum_reward/(ne+1))
+        print("\nrunning mean: ", sum_reward/(ne+1))
 
     print("mean ret: ", sum_reward/num_episode)
