a379789d90ed056c8ee3f6c2fbaf154416b48366
minaek <mk898@cornell.edu>
2020-02-09 23:08:06 -0800

    lqr code

--- lqr.py
+++ lqr.py
@@ -14,6 +14,7 @@ import numpy as np
 import scipy.linalg as la
 import sklearn
 from sklearn import datasets
+import scipy
 
 SHOW_ANIMATION = True
 
@@ -27,6 +28,7 @@ class LQRPlanner:
         self.MAX_ITER = 150
         self.EPS = 0.01
         self.init_system_models()
+        self.time = 0.0
 
     def init_system_models(self):
         """
@@ -37,22 +39,23 @@ class LQRPlanner:
         self.B = np.array([0.0, 1.0]).reshape(2, 1)
         self.R = np.eye(1)
         Q1 = np.array([[ 2.33035357, -0.81803898], [-0.81803898,  1.17972475]])
-        Q2 = np.eye(2)
         #self.Q = sklearn.datasets.make_spd_matrix(2, random_state=None)
         self.Q = Q1
-        print("Q: ", self.Q)
 
-    def lqr_planning(self, sx, sy, gx, gy, show_animation=True):
+    def lqr_planning(self, sx, sy, gx, gy, Q, show_animation=True):
 
         rx, ry = [sx], [sy]
 
         x = np.array([sx - gx, sy - gy]).reshape(2, 1)  # State vector
 
         found_path = False
+        self.Q = Q
+        print("\nQ: ", self.Q)
 
-        time = 0.0
-        while time <= self.MAX_TIME:
-            time += self.DT
+        self.time = 0.0
+        while self.time <= self.MAX_TIME:
+            self.time += self.DT
+            #print("--Time: ", self.time)
 
             u = self.lqr_control(x)
 
@@ -60,6 +63,11 @@ class LQRPlanner:
 
             rx.append(x[0, 0] + gx)
             ry.append(x[1, 0] + gy)
+            #if time == self.DT:
+            #    print("Ax: ", self.A@x)
+            #    print("Bu: ", self.B@u)
+            #    print("x_t+1: ", x)
+            #    print("posx, posy: ", x[0,0] + gx, x[1,0] + gy)
 
             d = math.sqrt((gx - rx[-1]) ** 2 + (gy - ry[-1]) ** 2)
             if d <= self.GOAL_DIST:
@@ -90,8 +98,18 @@ class LQRPlanner:
         X, Xn = self.Q, self.Q
 
         for i in range(self.MAX_ITER):
-            Xn = self.A.T * X * self.A - self.A.T * X * self.B * \
-                 la.inv(self.R + self.B.T * X * self.B) * self.B.T * X * self.A + self.Q
+            Xn = self.A.T @ X @ self.A - self.A.T @ X * self.B * \
+                 la.inv(self.R + self.B.T @ X @ self.B) * self.B.T @ X @ self.A + self.Q
+            print("\nAA: ", self.A.T@self.A)
+            print("APA: ", self.A.T@X@self.A)
+            print("BPA: ", self.B.T@X@self.A)
+            print("inv: ", la.inv(self.R + self.B.T @ X @ self.B) )
+            print("inv*BA: ", la.inv(self.R + self.B.T @ X @ self.B) * self.B.T @ X @ self.A )
+            print("AB*inv*BA: ", self.A.T @ X * self.B * \
+                 la.inv(self.R + self.B.T @ X @ self.B) * self.B.T @ X @ self.A )
+            print("evthing cept Q: ", self.A.T @ X @ self.A - self.A.T @ X * self.B * \
+                 la.inv(self.R + self.B.T @ X @ self.B) * self.B.T @ X @ self.A)
+            print("Pt-1: ", Xn, i)
             if (abs(Xn - X)).max() < self.EPS:
                 break
             X = Xn
@@ -106,11 +124,19 @@ class LQRPlanner:
         """
 
         # first, try to solve the ricatti equation
-        X = self.solve_dare()
+        #X1 = self.solve_dare()
+        #print("X1: ", X1)
+        X = np.matrix(scipy.linalg.solve_discrete_are(self.A, self.B, self.Q, self.R))
+        if self.time == self.DT: print("X: ", X)
 
         # compute the LQR gain
-        K = la.inv(self.B.T @ X @ self.B + self.R) @ (self.B.T @ X @ self.A)
-
+        #K = la.inv(self.B.T @ X @ self.B + self.R) @ (self.B.T @ X @ self.A)
+        K = np.matrix(scipy.linalg.inv(self.B.T * X * self.B + self.R) * (self.B.T * X * self.A))
+        #print("BXA: ", self.B.T * X * self.A)
+        #print("BXB: ",self.B.T * X * self.B)
+        #print("inv: ", scipy.linalg.inv(self.B.T * X * self.B + self.R))
+        #print("inv*BXA: ", scipy.linalg.inv(self.B.T * X * self.B + self.R) * (self.B.T * X * self.A))
+        if self.time == self.DT: print("K: ", K)
         eigValues = la.eigvals(self.A - self.B @ K)
 
         return K, X, eigValues
@@ -121,7 +147,8 @@ class LQRPlanner:
 
         Kopt, X, ev = self.dlqr()
 
-        u = -Kopt @ x
+        u = -Kopt @ x  # normal
+        #print("u: ", u)
 
         return u
 
@@ -129,27 +156,36 @@ class LQRPlanner:
 def main():
     print(__file__ + " start!!")
 
-    #ntest = 10  # number of goal
+    ntest = 10  # number of goal
     #area = 100.0  # sampling area
 
     lqr_planner = LQRPlanner()
+    Qs = [np.eye(2)*0.5, np.eye(2), np.eye(2)*2, np.eye(2)*3, np.eye(2)*4, np.eye(2)*5]
+    #Qs = [np.array([[ 1. , -0.9],
+    #                [-0.9,  1. ]]),
+    #    np.array([[ 1. , -0.5],
+    #            [-0.5,  1. ]]),
+    #    np.array([[1. , 0.5],
+    #            [0.5, 1. ]]),
+    #    np.array([[1. , 0.9],
+    #            [0.9, 1. ]])]
 
-    #for i in range(ntest):
-    sx = 0.0
-    sy = 0.0
-    #gx = random.uniform(-area, area)
-    #gy = random.uniform(-area, area)
-    gx = 50.
-    gy = 50.
+    for i in range(len(Qs)):
+        sx = 0.0
+        sy = 0.0
+        #gx = random.uniform(-area, area)
+        #gy = random.uniform(-area, area)
+        gx = 50.
+        gy = 50.
 
-    rx, ry = lqr_planner.lqr_planning(sx, sy, gx, gy, show_animation=SHOW_ANIMATION)
+        rx, ry = lqr_planner.lqr_planning(sx, sy, gx, gy, Qs[i], show_animation=SHOW_ANIMATION)
 
-    if SHOW_ANIMATION:  # pragma: no cover
-        plt.plot(sx, sy, "or")
-        plt.plot(gx, gy, "ob")
-        plt.plot(rx, ry, "-r")
-        plt.axis("equal")
-        plt.pause(1.0)
+        if SHOW_ANIMATION:  # pragma: no cover
+            plt.plot(sx, sy, "or")
+            plt.plot(gx, gy, "ob")
+            plt.plot(rx, ry, "-r")
+            plt.axis("equal")
+            plt.pause(1.0)
 
 
 if __name__ == '__main__':
