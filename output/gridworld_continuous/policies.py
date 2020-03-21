3d794a43dc800b2f9cddff7f8adb06c93f90d712
minaek <mk898@cornell.edu>
2020-03-22 00:12:25 +0900

    new policies

--- output/gridworld_continuous/policies.py
+++ output/gridworld_continuous/policies.py
@@ -1,7 +1,9 @@
-B0R = ["output/gridworld_continuous", "B0R", "model_219904_60.783292084955555.pkl"]
+#B0R = ["output/gridworld_continuous", "B0R", "model_219904_60.783292084955555.pkl"]
+B0R = ["output/gridworld_continuous", "B0R", "model_206720_58.74888285090978.pkl"]
 B0L = ["output/gridworld_continuous", "B0L", "model_219904_59.10693766311562.pkl"]
 
-B1R = ["output/gridworld_continuous", "B1R", "model_219136_60.892298539093844.pkl"]
+#B1R = ["output/gridworld_continuous", "B1R", "model_219136_60.892298539093844.pkl"]
+B1R = ["output/gridworld_continuous", "B1R", "model_210688_61.11581044997948.pkl"]
 B1L = ["output/gridworld_continuous", "B1L", "model_219904_60.91510483141668.pkl"]
 
 B2R = ["output/gridworld_continuous", "B2R", "model_219904_59.897271101985176.pkl"]
