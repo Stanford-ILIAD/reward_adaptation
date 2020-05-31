a2c5ec6785298532df0d9fa29c437fc92bfafd47
minaek <mk898@cornell.edu>
2020-06-01 07:10:05 +0900

    updated policies file for nav

--- output/updated_gridworld_continuous/policies.py
+++ output/updated_gridworld_continuous/policies.py
@@ -1,11 +1,25 @@
 B0R = ["output/updated_gridworld_continuous", "B0R", "best_model_14336_62.39190789136612.pkl"]
 B1R = ["output/updated_gridworld_continuous", "B1R", "best_model_215808_62.22815662179966.pkl"]
+B1R1 = ["output/updated_gridworld_continuous", "B1R", "model_216064_59.8557838740874.pkl"]
+B1R2 = ["output/updated_gridworld_continuous", "B1R", "model_216320_60.77716059383434.pkl"]
+
 B2R = ["output/updated_gridworld_continuous", "B2R", "best_model_120576_60.402674526604876.pkl"]
+
 B3R = ["output/updated_gridworld_continuous", "B3R", "best_model_124032_62.29229838319685.pkl"]
+B3R1 = ["output/updated_gridworld_continuous", "B3R", "model_130432_60.042624844634.pkl"]
+B3R2 = ["output/updated_gridworld_continuous", "B3R", "model_214912_61.172637044374284.pkl"]
+
 B4R = ["output/updated_gridworld_continuous", "B4R", "best_model_24064_55.268307150844656.pkl"]
 B6R = ["output/updated_gridworld_continuous", "B6R", "best_model_23936_54.54799440976602.pkl"]
+
 B5R = ["output/updated_gridworld_continuous", "B5R", "best_model_135168_62.205550367107705.pkl"]
+B5R1 = ["output/updated_gridworld_continuous", "B5R", "model_134656_54.28355803991594.pkl"]
+B5R2 = ["output/updated_gridworld_continuous", "B5R", "model_135936_54.65200649011712.pkl"]
+
 B7R = ["output/updated_gridworld_continuous", "B7R", "best_model_160128_53.80273751182808.pkl"]
+B7R1 = ["output/updated_gridworld_continuous", "B7R", "model_27904_53.0769959331807.pkl"]
+B7R2 = ["output/updated_gridworld_continuous", "B7R", "model_59648_53.060592416888205.pkl"]
+
 B9R = ["output/updated_gridworld_continuous", "B9R", "best_model_121600_72.42990690191905.pkl"]
 
 
@@ -19,14 +33,27 @@ B7L = ["output/updated_gridworld_continuous", "B7L", "best_model_131456_53.17251
 
 
 B1R_B0L = ["output/updated_gridworld_continuous", "B1R_B0L", "best_model_128_57.502501283889494.pkl"]
+B1R_B0L1 = ["output/updated_gridworld_continuous", "B1R_B0L", "model_210560_57.34727342208144.pkl"]
+B1R_B0L2 = ["output/updated_gridworld_continuous", "B1R_B0L", "model_71040_56.270906617086126.pkl"]
+
 B2R_B0L = ["output/updated_gridworld_continuous", "B2R_B0L", "best_model_48640_60.44307513964023.pkl"]
+
 B3R_B0L = ["output/updated_gridworld_continuous", "B3R_B0L", "best_model_84480_62.28856462807559.pkl"]
+B3R_B0L1 = ["output/updated_gridworld_continuous", "B3R_B0L", "model_98560_62.20306929126655.pkl"]
+B3R_B0L2 = ["output/updated_gridworld_continuous", "B3R_B0L", "model_87808_59.98765780964113.pkl"]
+
 B4R_B0L = ["output/updated_gridworld_continuous", "B4R_B0L", "best_model_20480_54.83134508513394.pkl"]
+
 B5R_B0L = ["output/updated_gridworld_continuous", "B5R_B0L", "best_model_51200_63.06722307033304.pkl"]
+B5R_B0L1 = ["output/updated_gridworld_continuous", "B5R_B0L", "model_67200_62.20520720207253.pkl"]
+B5R_B0L2 = ["output/updated_gridworld_continuous", "B5R_B0L", "model_163456_61.202756393451814.pkl"]
+
 B6R_B0L = ["output/updated_gridworld_continuous", "B6R_B0L", "best_model_41728_57.29504647332107.pkl"]
+
 B7R_B0L = ["output/updated_gridworld_continuous", "B7R_B0L", "best_model_14336_63.814472179219116.pkl"]
+B7R_B0L1 = ["output/updated_gridworld_continuous", "B7R_B0L", "model_240512_56.08853564493836.pkl"]
+B7R_B0L2 = ["output/updated_gridworld_continuous", "B7R_B0L", "model_103040_56.10295177947137.pkl"]
 
 
 B5R_B5L = ["output/updated_gridworld_continuous", "B5R_B5L", "best_model_9984_59.27201191236718.pkl"]
 B5R_B0L_B5L = ["output/updated_gridworld_continuous", "B5R_B0L_B5L", "best_model_48128_56.34691048530898.pkl"]
-
