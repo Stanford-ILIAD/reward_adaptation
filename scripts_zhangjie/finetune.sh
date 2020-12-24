ff9a5512a26b1acf2211126978fe037fdf9f3177
Minae Kwon <mk898@cornell.edu>
2020-12-24 00:01:32 -0800

    scripts

--- scripts_zhangjie/finetune.sh
+++ scripts_zhangjie/finetune.sh
@@ -4,11 +4,11 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4G
 #SBATCH --gres=gpu:1 
-#SBATCH --job-name="spours_bs7"
+#SBATCH --job-name="fetch_finetune"
 #SBATCH --output=/iliad/u/minae/reward_adaptation/jobs/%x.o
 
 ##CUDA_VISIBLE_DEVICES=0 python train.py --env nav1 --expt_type finetune --bs 7
-##CUDA_VISIBLE_DEVICES=0 python train.py --env nav1_sparse --bs 7 --experiment_dir output/sparse --expt_type ours
+CUDA_VISIBLE_DEVICES=0 python train.py --env fetch --expt_type finetune
 ###CUDA_VISIBLE_DEVICES=0 python baselines/PNN/train.py --bs 7
 ###CUDA_VISIBLE_DEVICES=0 python baselines/L2SP/train.py --bs 5
 ###CUDA_VISIBLE_DEVICES=0 python baselines/BSS/train.py --bs 7
