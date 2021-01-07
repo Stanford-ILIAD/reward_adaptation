fa81b5beab21608b4cd2c1a36d8b41339b2ea607
Minae Kwon <mk898@cornell.edu>
2021-01-06 19:29:34 -0800

    updating files

--- driving/driving_envs/envs/__init__.py
+++ driving/driving_envs/envs/__init__.py
@@ -10,3 +10,21 @@ GridworldContinuousMultiObjLLEnv, GridworldContinuousMultiObjRREnv, \
 GridworldContinuousMultiObjLREnv, GridworldContinuousMultiObjRLEnv, \
 GridworldContinuousNoneLLEnv, GridworldContinuousNoneRREnv, \
 GridworldContinuousNoneLREnv, GridworldContinuousNoneRLEnv, GridworldSparseEnv
+
+from driving_envs.envs.merging_env import MergingEnv, MergingEnv2, MergingEnv3, MergingEnv4, MergingEnv5,\
+    MergingEnv6, MergingEnv7, MergingEnv8, MergingEnv9, MergingEnv10, MergingEnv11
+
+from driving_envs.envs.obstacle_avoidance_env import ObstacleAvoidanceEnv, \
+ObstacleAvoidanceEnv2, ObstacleAvoidanceEnv3, ObstacleAvoidanceEnv4, ObstacleAvoidanceEnv5, \
+ObstacleAvoidanceEnv6, ObstacleAvoidanceEnv7, ObstacleAvoidanceEnv8, ObstacleAvoidanceEnv9
+
+from driving_envs.envs.gridworld_continuous import GridworldContinuousEnv, \
+GridworldContinuousMultiObjLLEnv, GridworldContinuousMultiObjRREnv, \
+GridworldContinuousMultiObjLREnv, GridworldContinuousMultiObjRLEnv, \
+GridworldContinuousNoneLLEnv, GridworldContinuousNoneRREnv, \
+GridworldContinuousNoneLREnv, GridworldContinuousNoneRLEnv, \
+GridworldContinuousAdjustLREnv, GridworldContinuousAdjustRLEnv, \
+GridworldContinuousAdjustLLEnv, GridworldContinuousAdjustRREnv, \
+GridworldContinuousAdjustRLREnv, GridworldContinuousAdjustRRLEnv, \
+GridworldContinuousAdjustRLLEnv, GridworldContinuousAdjustRRREnv
+
